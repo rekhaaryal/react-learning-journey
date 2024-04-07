ac2f342616ee11d2e7830be0683c6a82ca186f97
rekhaaryal <aryalr458@gmail.com>
2024-04-07 16:36:05 +0545

    Props in react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,16 +3,20 @@
 // import Footer from './Footer.jsx'
 // import Food from './Food.jsx'
 // import Card from './Card.jsx'
-import Button from './Button/Button.jsx'
-
+// import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 function App() {
   
-  return(<Button/>);
-    // <>
-  // <Button/>
-  // </>
-    
-  
+  return(
+    <>
+      <Student name="Rekha Aryal" age={23} isStudent={true}/>
+      <Student name="Rama Aryal" age={25} isStudent={true}/>
+      <Student name="Roshan Aryal" age={20} isStudent={true}/>
+      <Student name="kalpana Aryal" age={47} isStudent={false }/>
+      <Student name="Tej prasad  Aryal" age={50} isStudent={false}/>
+      <Student/>
+    </>
+  );
 }
 
 export default App
