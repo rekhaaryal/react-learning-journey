517a5fedb7a8fd3eef120d46b4c649113f1161f7
rekhaaryal <aryalr458@gmail.com>
2024-04-07 19:04:16 +0545

    conditional render in react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,19 +4,39 @@
 // import Food from './Food.jsx'
 // import Card from './Card.jsx'
 // import Button from './Button/Button.jsx'
-import Student from './Student.jsx'
-function App() {
+
+
+
+//4. PROPS in react 
+
+// import Student from './Student.jsx'
+// function App() {
   
+//   return(
+//     <>
+//       <Student name="Rekha Aryal" age={23} isStudent={true}/>
+//       <Student name="Rama Aryal" age={25} isStudent={true}/>
+//       <Student name="Roshan Aryal" age={20} isStudent={true}/>
+//       <Student name="kalpana Aryal" age={47} isStudent={false }/>
+//       <Student name="Tej prasad  Aryal" age={50} isStudent={false}/>
+//       <Student/>
+//     </>
+//   );
+// }
+
+// export default App
+
+
+//5. How to conditional reader in react
+import UserGreeting from './UserGreeting.jsx';
+
+function App(){
+
   return(
     <>
-      <Student name="Rekha Aryal" age={23} isStudent={true}/>
-      <Student name="Rama Aryal" age={25} isStudent={true}/>
-      <Student name="Roshan Aryal" age={20} isStudent={true}/>
-      <Student name="kalpana Aryal" age={47} isStudent={false }/>
-      <Student name="Tej prasad  Aryal" age={50} isStudent={false}/>
-      <Student/>
+      <UserGreeting isLoggedIn={false} username="Rekha Aryal" />
     </>
   );
-}
 
+}
 export default App
